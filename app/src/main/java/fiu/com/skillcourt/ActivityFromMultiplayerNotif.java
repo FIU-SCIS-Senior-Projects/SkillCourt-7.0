91762e3fb8aa4ef077a0c381e278246af91a89b6
alvarosilvac <alvarosilvacas@gmail.com>
2017-04-15 18:20:26 -0400

    last update

--- app/src/main/java/fiu/com/skillcourt/ActivityFromMultiplayerNotif.java
+++ app/src/main/java/fiu/com/skillcourt/ActivityFromMultiplayerNotif.java
@@ -2,12 +2,28 @@ package fiu.com.skillcourt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import fiu.com.skillcourt.ui.base.BaseActivity;
+import fiu.com.skillcourt.ui.base.BaseFragment;
 
 public class ActivityFromMultiplayerNotif extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_from_multiplayer_notif);
+
+        Button acceptButton = (Button) findViewById(R.id.accept_button);
+
+        acceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("working?", "working!");
+            }
+        });
     }
 }
